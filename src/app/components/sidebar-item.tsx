b74cac2d8d10ceb4ded50d7f00706d47eb28dbc4
prafessor <olegmilyo@gmail.com>
2025-10-27 13:52:35 +0200

    Set navigation

--- src/app/components/sidebar-item.tsx
+++ src/app/components/sidebar-item.tsx
@@ -1,7 +1,10 @@
+import clsx from 'clsx';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 export interface SidebarItemProps {
+  current?: boolean;
   pathname: string;
   src: string;
   alt: string;
@@ -9,6 +12,7 @@ export interface SidebarItemProps {
 }
 
 export default function SidebarItem({
+  current,
   pathname,
   src,
   alt,
@@ -16,10 +20,17 @@ export default function SidebarItem({
 }: SidebarItemProps) {
   return (
     <li>
-      <a className='flex items-center gap-3.5 h-9 mx-1' href={pathname}>
-        <Image className='ml-5' width={18} height={18} src={src} alt={alt} />
-        <span className='font-medium text-zinc-50'>{children}</span>
-      </a>
+      <Link
+        className={clsx(
+          'flex items-center gap-3.5 h-9 mx-1',
+          current &&
+            'after:h-full after:ml-auto after:border-2 after:border-purple-200 after:rounded-sm',
+        )}
+        href={pathname}
+      >
+        <Image className="ml-5" width={18} height={18} src={src} alt={alt} />
+        <span className="font-medium text-zinc-50">{children}</span>
+      </Link>
     </li>
   );
 }

96447d5541612f178d4da4175ff74758fe7a5f22
prafessor <olegmilyo@gmail.com>
2025-11-03 17:37:35 +0200

    Set route in companies page

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,24 +1,16 @@
 'use client';
 
-import React, { useEffect } from "react";
+import React from "react";
 import Header from "@/app/components/header";
-import { notFound } from "next/navigation";
 
 export interface PageProps {
     params: { id: string };
 }
 
 export default function Page({ params }:PageProps) {
-    useEffect(() => {
-        const id = Number.parseInt(params.id);
-        if(Number.isNaN(id)) {
-            notFound();
-        }
-    }, [params.id])
-
     return (
-        <>
-            <Header>Companies ({params.id})</Header>
-        </>
+        <div className="py-6 px-10">
+            <p>{`Information about company (${params.id})`}</p>
+        </div>
     );
 }
--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -1,24 +1,15 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import Button from './button';
-import dynamic from 'next/dynamic';
-// import CompanyFormModal from "./company-form-modal";
-
-const CompanyFormModal = dynamic(() => import('./company-form-modal'), {
-  ssr: false,
-});
+import { useRouter } from 'next/navigation';
 
 export default function AddCompanyButton() {
-  const [show, setShow] = useState(false);
+  const router = useRouter();
+
   return (
     <>
-      <Button onClick={() => setShow(true)}>Add company</Button>
-      <CompanyFormModal
-        onSubmit={console.log}
-        show={show}
-        onClose={() => setShow(false)}
-      />
+      <Button onClick={() => router.push('/companies/new')}>Add company</Button>
     </>
   );
 }
